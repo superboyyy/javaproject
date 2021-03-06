7a68b726ff6ba1aaad19d8e30fa6711f29edf7eb
skydyboy <superboyyy6@gmail.com>
2021-03-06 18:16:45 +0800

    java project

--- chapter9/src/linearequation/LinearEquation.java
+++ chapter9/src/linearequation/LinearEquation.java
@@ -1,4 +1,53 @@
 package linearequation;
 
 public class LinearEquation {
+    private final double a, b, c, d, e, f;
+
+    LinearEquation(double A, double B, double C, double D, double E, double F) {
+        a = A;
+        b = B;
+        c = C;
+        d = D;
+        e = E;
+        f = F;
+    }
+
+    public double getA() {
+        return a;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public double getC() {
+        return c;
+    }
+
+    public double getD() {
+        return d;
+    }
+
+    public double getE() {
+        return e;
+    }
+
+    public double getF() {
+        return f;
+    }
+
+    public boolean isSolvable() {
+        if (a * d - b * c != 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public double getX() {
+        return (e * d - b * f) / (a * d - b * c);
+    }
+
+    public double getY() {
+        return (a * f - e * c) / (a * d - b * c);
+    }
 }
---
+++ chapter9/src/linearequation/TestLinearEquation.java
@@ -0,0 +1,23 @@
+package linearequation;
+
+import java.util.Scanner;
+
+public class TestLinearEquation {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please enter the value of a,b,c,d,e,f. After enter a value, press Enter.");
+        double a = in.nextDouble();
+        double b = in.nextDouble();
+        double c = in.nextDouble();
+        double d = in.nextDouble();
+        double e = in.nextDouble();
+        double f = in.nextDouble();
+
+        LinearEquation linearEquation = new LinearEquation(a,b,c,d,e,f);
+
+        if (linearEquation.isSolvable())
+            System.out.println("x is " + linearEquation.getX() + " y is " + linearEquation.getY());
+        else
+            System.out.println("The equation has no solution.");
+    }
+}
