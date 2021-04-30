d586a95b3f040fa12fb88d23da3db2bc687c000f
skydyboy <superboyyy6@gmail.com>
2021-04-30 18:00:18 +0800

    Java Project

---
+++ chapter11/src/matrix/Matrix.java
@@ -0,0 +1,13 @@
+//package matrix;
+//
+//import java.util.Scanner;
+//
+//public class Matrix {
+//    public static void main(String[] args) {
+//        System.out.print("Enter the array size n: ");
+//        Scanner in = new Scanner(System.in);
+//        int n = in.nextInt();
+//
+//        System.out.print
+//    }
+//}
